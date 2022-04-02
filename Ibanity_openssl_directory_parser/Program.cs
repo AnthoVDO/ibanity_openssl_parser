e8789a54a4aa576fda77c2f4535b0f90b4e4d51a
Antho VDO Novu <43015083+AnthoVDO@users.noreply.github.com>
2022-04-02 13:41:40 +0200

    first commit

---
+++ Ibanity_openssl_directory_parser/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting");
+Console.WriteLine("Please add openssl command");
+string ibanityCommand = Console.ReadLine();
+Console.WriteLine("Please add the folder location");
+string folderLaction = Console.ReadLine();
+
+
+string formated;
+
+Console.WriteLine(formated);
