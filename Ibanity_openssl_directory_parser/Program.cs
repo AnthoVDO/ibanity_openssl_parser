8e5855a97f31bd3aaa00801a147f35cb23431107
Antho VDO Novu <43015083+AnthoVDO@users.noreply.github.com>
2022-04-02 20:41:19 +0200

    I continued to program

--- Ibanity_openssl_directory_parser/Program.cs
+++ Ibanity_openssl_directory_parser/Program.cs
@@ -1,11 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+String rsaKey = "genrsa -aes256 -out private_key.pem 2048";
+
+
 Console.WriteLine("Starting");
-Console.WriteLine("Please add openssl command");
+Console.WriteLine("Do you want to generate an rsa key (step1)? y/n: ");
+
+string ansRsaKey = "";
+bool correctAns = false;
+while(!correctAns)
+{
+    ansRsaKey = Console.ReadLine();
+    if(ansRsaKey == "y" || ansRsaKey == "n")
+    {
+        correctAns = true;
+    }
+}
+    if(ansRsaKey == "y")
+    {
+        
+    } 
+
+
+
+
+
+
+
 string ibanityCommand = Console.ReadLine();
-Console.WriteLine("Please add the folder location");
+Console.WriteLine("Please add the folder location: ");
 string folderLaction = Console.ReadLine();
 
 
 string formated;
 
 Console.WriteLine(formated);
+
+//Step 1 generate the rsaKey
+
+//Step 2 Generate the Certificate Signing Request
+
+//Step 3 Generate the private key
