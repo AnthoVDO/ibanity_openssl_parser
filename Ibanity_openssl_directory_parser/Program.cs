367d0419c346deacf82538ae766f51a8e157b6b3
Antho VDO Novu <43015083+AnthoVDO@users.noreply.github.com>
2022-04-04 18:50:28 +0200

    I continue the key generator

--- Ibanity_openssl_directory_parser/Program.cs
+++ Ibanity_openssl_directory_parser/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
+static string cleanThePath(string path)
+{
+    path = path.Replace("\\", "/");
+    return path+"/";
+}
 
-String rsaKey = "genrsa -aes256 -out private_key.pem 2048";
+
 
 
 Console.WriteLine("Starting");
@@ -10,8 +15,11 @@ Console.WriteLine("Starting");
 //Select folder
 Console.WriteLine("Please enter a folder path: ");
 string folderPath = Console.ReadLine();
+folderPath = cleanThePath(folderPath);
+Console.WriteLine("Folder path set on: "+ folderPath);
+String rsaKey = $"genrsa -aes256 -out {folderPath}private_key.pem 2048";
 
-//Check for Rsa key
+//Step 1 generate the rsaKey
 Console.WriteLine("Do you want to generate an rsa key (step1)? y/n: ");
 
 string ansRsaKey = "";
@@ -31,25 +39,37 @@ while(!correctAns)
     if(ansRsaKey == "y")
     {
     Process.Start("C:/Program Files/Git/usr/bin/openssl.exe", rsaKey);
-    } 
+    }
 
-
-
-
-
-
-
-string ibanityCommand = Console.ReadLine();
-Console.WriteLine("Please add the folder location: ");
-string folderLaction = Console.ReadLine();
-
-
-string formated;
-
-//Console.WriteLine(formated);
-
-//Step 1 generate the rsaKey
 
 //Step 2 Generate the Certificate Signing Request
+
+Console.WriteLine("Do you want to generate signature key (step1)? y/n: ");
+
+string signatureKey = "";
+bool correctAnsSignature = false;
+while (!correctAns)
+{
+    ansRsaKey = Console.ReadLine();
+    if (signatureKey == "y" || signatureKey == "n")
+    {
+        correctAnsSignature = true;
+    }
+    else
+    {
+        Console.WriteLine("You need to answer correctly");
+    }
+}
+if (signatureKey == "y")
+{
+    Console.WriteLine("Please enter the ibanity signature command");
+    string ibanitySignatureCommand = Console.ReadLine();
+
+    Process.Start("C:/Program Files/Git/usr/bin/openssl.exe", rsaKey);
+}
+
+
+
+
 
 //Step 3 Generate the private key
